e50a910d2ac646db1aaefbf51083cfecff8e5df4
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2023-12-08 23:33:15 -0600

    Solved day 9

--- src/com/noahgeren/adventofcode/Launcher.java
+++ src/com/noahgeren/adventofcode/Launcher.java
@@ -4,7 +4,7 @@ public class Launcher {
 
 	private static final String PACKAGE = "xxiii";
 	private static final int YEAR = 2023;
-	private static final int DAY = 8;
+	private static final int DAY = 9;
 	
 	public static void main(String[] args) {
 		try {
---
+++ src/com/noahgeren/adventofcode/problems/xxiii/Day09.java
@@ -0,0 +1,45 @@
+package com.noahgeren.adventofcode.problems.xxiii;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.noahgeren.adventofcode.Day;
+import com.noahgeren.adventofcode.data.DataLoader;
+
+public class Day09 extends Day {
+	
+	private List<String> lines;
+
+	@Override
+	public void loadResources() throws Exception {
+		lines  = DataLoader.readLines("day09.txt");
+	}
+
+	@Override
+	public String solve(boolean firstPart) throws Exception {
+		long answer = 0;
+		for(String line : lines) {
+			String[] split = line.split(" ");
+			List<Long> sequence = Arrays.stream(split).map(Long::valueOf).collect(Collectors.toList());
+			answer += findNext(sequence, firstPart);
+		}
+		return String.valueOf(answer);
+	}
+	
+	private long findNext(List<Long> sequence, boolean firstPart) {
+		if(sequence.stream().allMatch(l -> l == 0)) {
+			return 0;
+		}
+		List<Long> diff = new ArrayList<>();
+		for(int i = 0; i < sequence.size() - 1; i++) {
+			diff.add(sequence.get(i + 1) - sequence.get(i));
+		}
+		if(firstPart) {
+			return sequence.get(sequence.size() - 1) + findNext(diff, firstPart);
+		}
+		return sequence.get(0) - findNext(diff, firstPart);
+	}
+
+}
