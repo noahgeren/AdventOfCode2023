9adf04da8fa42b9229005f0c018409b815ef8ff0
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2023-12-12 17:25:50 -0600

    Attempt at solving part 2 of day 12

--- src/com/noahgeren/adventofcode/problems/xxiii/Day12.java
+++ src/com/noahgeren/adventofcode/problems/xxiii/Day12.java
@@ -10,12 +10,14 @@ public class Day12 extends Day {
 
 	char[][] arrangements;
 	int[][] counts;
+	long[][] arrangementCounts;
 
 	@Override
 	public void loadResources() throws Exception {
 		List<String> linesTemp = DataLoader.readLines("day12.txt");
 		arrangements = new char[linesTemp.size()][];
 		counts = new int[linesTemp.size()][];
+		arrangementCounts = new long[arrangements.length][];
 		for (int i = 0; i < linesTemp.size(); i++) {
 			String[] line = linesTemp.get(i).split(" ");
 			arrangements[i] = line[0].toCharArray();
@@ -24,16 +26,48 @@ public class Day12 extends Day {
 			for (int j = 0; j < strCounts.length; j++) {
 				counts[i][j] = Integer.valueOf(strCounts[j]);
 			}
+			arrangementCounts[i] = new long[]{0, 0};
 		}
 	}
 
 	@Override
 	public String solve(boolean firstPart) throws Exception {
+		if(!firstPart) {
+			char[][] newArrangements = new char[arrangements.length][];
+			for(int i = 0; i < arrangements.length; i++) {
+				newArrangements[i] = new char[arrangements[i].length * 2 + 1];
+				for(int j = 0; j < arrangements[i].length; j++) {
+					newArrangements[i][j] = arrangements[i][j];
+					newArrangements[i][j + arrangements[i].length + 1] = arrangements[i][j];
+				}
+				newArrangements[i][arrangements[i].length] = '?';
+			}
+			arrangements = newArrangements;
+			int[][] newCounts = new int[counts.length][];
+			for(int i = 0; i < counts.length; i++) {
+				newCounts[i] = new int[counts[i].length * 2];
+				for(int j = 0; j < counts[i].length; j++) {
+					newCounts[i][j] = counts[i][j];
+					newCounts[i][j + counts[i].length] = counts[i][j];
+				}
+			}
+			counts = newCounts;
+		}
+		
 		long totalArrangements = 0;
 
 		for (int i = 0; i < arrangements.length; i++) {
+			System.out.println((i + 1) + "/" + arrangements.length);
 			long count = countArrangements(arrangements[i], counts[i], 0);
+			arrangementCounts[i][firstPart ? 0 : 1] = count;
+			if(!firstPart) {
+				long ratio = arrangementCounts[i][1] / arrangementCounts[i][0];
+				for(int j = 0; j < 3; j++) {
+					count *= ratio;
+				}
+			}
 			totalArrangements += count;
+//			System.out.println(count);
 		}
 
 		return String.valueOf(totalArrangements);
