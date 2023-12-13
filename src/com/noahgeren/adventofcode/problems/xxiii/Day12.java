edca698dec55f2b3ea5c8b4d3eb00e0b1c14d236
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2023-12-13 15:05:36 -0600

    Done with day 12 for now

--- src/com/noahgeren/adventofcode/problems/xxiii/Day12.java
+++ src/com/noahgeren/adventofcode/problems/xxiii/Day12.java
@@ -9,8 +9,9 @@ import com.noahgeren.adventofcode.Day;
 import com.noahgeren.adventofcode.data.DataLoader;
 
 public class Day12 extends Day {
-	
+
 	private final int DUPLICATE = 5;
+	private final int MAX = 15;
 
 	char[][] arrangements;
 	int[][] counts;
@@ -21,7 +22,9 @@ public class Day12 extends Day {
 		List<String> linesTemp = DataLoader.readLines("day12.txt");
 		arrangements = new char[linesTemp.size()][];
 		counts = new int[linesTemp.size()][];
-		arrangementCounts = new BigInteger[arrangements.length][];
+		if(arrangementCounts == null) {
+			arrangementCounts = new BigInteger[arrangements.length][];
+		}
 		for (int i = 0; i < linesTemp.size(); i++) {
 			String[] line = linesTemp.get(i).split(" ");
 			arrangements[i] = line[0].toCharArray();
@@ -30,80 +33,72 @@ public class Day12 extends Day {
 			for (int j = 0; j < strCounts.length; j++) {
 				counts[i][j] = Integer.valueOf(strCounts[j]);
 			}
-			arrangementCounts[i] = new BigInteger[2];
+			if(arrangementCounts[i] == null) {
+				arrangementCounts[i] = new BigInteger[DUPLICATE];
+			}
 		}
 	}
 
 	@Override
 	public String solve(boolean firstPart) throws Exception {
-		if (!firstPart) {
-			char[][] newArrangements = new char[arrangements.length][];
-			for (int i = 0; i < arrangements.length; i++) {
-				List<Character> line = new ArrayList<>();
-				for(int k = 0; k < DUPLICATE; k++) {
-					for (int j = 0; j < arrangements[i].length; j++) {
-						line.add(arrangements[i][j]);
-					}
-					if(k != DUPLICATE - 1) {
-						line.add('?');
-					}
-				}
-				newArrangements[i] = new char[line.size()];
-				for(int j = 0; j < line.size(); j++) {
-					newArrangements[i][j] = line.get(j);
-				}
-			}
-			arrangements = newArrangements;
-			
-			int[][] newCounts = new int[counts.length][];
-			for (int i = 0; i < counts.length; i++) {
-				List<Integer> line = new ArrayList<>();
-				for(int k = 0; k < DUPLICATE; k++) {
-					for (int j = 0; j < counts[i].length; j++) {
-						line.add(counts[i][j]);
-					}
-				}
-				newCounts[i] = new int[line.size()];
-				for(int j = 0; j < line.size(); j++) {
-					newCounts[i][j] = line.get(j);
-				}
-			}
-			counts = newCounts;
+		for(int i = 1; i <= DUPLICATE; i++) {
+			loadResources();
+			findForDuplicate(i);
 		}
+		for(int i = 0; i < arrangementCounts.length && i < MAX; i++) {
+			for(int j = 0; j < arrangementCounts[i].length; j++) {
+				System.out.print(arrangementCounts[i][j] + " ");
+			}
+			System.out.println();
+		}
+		return null;
+	}
+	
+	private void findForDuplicate(int duplicate) {
+		char[][] newArrangements = new char[arrangements.length][];
+		for (int i = 0; i < arrangements.length; i++) {
+			List<Character> line = new ArrayList<>();
+			for (int k = 0; k < duplicate; k++) {
+				for (int j = 0; j < arrangements[i].length; j++) {
+					line.add(arrangements[i][j]);
+				}
+				if (k != duplicate - 1) {
+					line.add('?');
+				}
+			}
+			newArrangements[i] = new char[line.size()];
+			for (int j = 0; j < line.size(); j++) {
+				newArrangements[i][j] = line.get(j);
+			}
+		}
+		arrangements = newArrangements;
 
+		int[][] newCounts = new int[counts.length][];
+		for (int i = 0; i < counts.length; i++) {
+			List<Integer> line = new ArrayList<>();
+			for (int k = 0; k < duplicate; k++) {
+				for (int j = 0; j < counts[i].length; j++) {
+					line.add(counts[i][j]);
+				}
+			}
+			newCounts[i] = new int[line.size()];
+			for (int j = 0; j < line.size(); j++) {
+				newCounts[i][j] = line.get(j);
+			}
+		}
+		counts = newCounts;
+		
 		BigInteger totalArrangements = BigInteger.ZERO;
-		for (int i = 0; i < arrangements.length && i < 18; i++) {
-			System.out.print((i + 1) + "/" + arrangements.length + ": ");
+		for (int i = 0; i < arrangements.length && i < MAX; i++) {
+			System.out.println(duplicate + " " + (i + 1) + "/" + arrangements.length);
 			BigInteger count = countArrangements(arrangements[i], counts[i], 0);
-			arrangementCounts[i][firstPart ? 0 : 1] = count;
-			if (!firstPart && DUPLICATE == 2) {
-				BigInteger ratio = arrangementCounts[i][1].divide(arrangementCounts[i][0]);
-				for (int j = 0; j < 3; j++) {
-					count = count.multiply(ratio);
-				}
-			}
+			arrangementCounts[i][duplicate - 1] = count;
 			totalArrangements = totalArrangements.add(count);
-			System.out.println(count);
-			if (!firstPart && DUPLICATE == 2 && arrangementCounts[i][1].mod(arrangementCounts[i][0]).compareTo(BigInteger.ZERO) != 0) {
-				System.out.println("here");
-//				System.out.println(arrangementCounts[i][1] + "%" + arrangementCounts[i][0] + "="
-//						+ arrangementCounts[i][1].mod(arrangementCounts[i][0]));
-
-			}
-//			break;
 		}
-		return String.valueOf(totalArrangements);
 	}
 
 	private BigInteger countArrangements(char[] arr, int[] counts, int index) {
 		if (index >= arr.length) {
-//			if (checkValidUpTo(arr, counts, index)) {
-//				System.out.println();
-//				for (char c : arr) {
-//					System.out.print(c);
-//				}
-//				System.out.println();
-//			}
 			return checkValidUpTo(arr, counts, index) ? BigInteger.ONE : BigInteger.ZERO;
 		}
 		if (arr[index] == '?') {
