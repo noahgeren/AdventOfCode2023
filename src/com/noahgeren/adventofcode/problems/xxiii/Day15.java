1000808f35b310da97a2bab743b4439390c49555
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2023-12-15 00:17:11 -0600

    Solved day 15

---
+++ src/com/noahgeren/adventofcode/problems/xxiii/Day15.java
@@ -0,0 +1,73 @@
+package com.noahgeren.adventofcode.problems.xxiii;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
+import com.noahgeren.adventofcode.Day;
+import com.noahgeren.adventofcode.data.DataLoader;
+
+public class Day15 extends Day {
+	
+	private String[] sequence;
+
+	@Override
+	public void loadResources() throws Exception {
+		sequence = DataLoader.readLines("day15.txt").get(0).split(",");
+	}
+
+	@Override
+	public String solve(boolean firstPart) throws Exception {
+		if(firstPart) {
+			return solveFirstPart();
+		}
+		return solveSecondPart();
+	}
+	
+	private String solveFirstPart() {
+		long sum = 0;
+		for(String str : sequence) {
+			sum += hash(str);
+		}
+		return String.valueOf(sum);
+	}
+	
+	private String solveSecondPart() {
+		BigInteger sum = BigInteger.ZERO;
+		HashMap<Long, LinkedHashMap<String, Long>> map = new HashMap<>();
+		for(String str : sequence) {
+			if(str.endsWith("-")) {
+				String label = str.substring(0, str.length() - 1);
+				long hash = hash(label);
+				map.computeIfAbsent(hash, (x) -> new LinkedHashMap<String, Long>()).remove(label);
+			} else {
+				String label = str.substring(0, str.length() - 2);
+				long hash = hash(label);
+				long focalLength = str.charAt(str.length() - 1) - '0';
+				map.computeIfAbsent(hash, (x) -> new LinkedHashMap<String, Long>()).put(label, focalLength);
+			}
+		}
+		for(Entry<Long, LinkedHashMap<String, Long>> entry : map.entrySet()) {
+			BigInteger box = BigInteger.valueOf(entry.getKey() + 1);
+			LinkedHashMap<String, Long> contents = entry.getValue();
+			BigInteger i = BigInteger.ONE;
+			for(Long focalLength : contents.values()) {
+				sum = sum.add(box.multiply(i).multiply(BigInteger.valueOf(focalLength)));
+				i = i.add(BigInteger.ONE);
+			}
+		}
+		return sum.toString();
+	}
+	
+	private long hash(String str) {
+		long hash = 0;
+		for(char c : str.toCharArray()) {
+			hash += c;
+			hash *= 17;
+			hash %= 256;
+		}
+		return hash;
+	}
+
+}
