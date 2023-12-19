4f2a96efb465f7346cb333afd5426408b8ad5923
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2023-12-18 23:14:20 -0600

    Added boilerplate for day 19
    Not going to start until tomorrow

---
+++ src/com/noahgeren/adventofcode/problems/xxiii/Day19.java
@@ -0,0 +1,19 @@
+package com.noahgeren.adventofcode.problems.xxiii;
+
+import com.noahgeren.adventofcode.Day;
+
+public class Day19 extends Day {
+
+	@Override
+	public void loadResources() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String solve(boolean firstPart) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
