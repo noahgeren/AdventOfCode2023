0b6ef3a3fe657b666ae5a270f5e9d989d68dc4d5
Noah Geren <40411652+noahgeren@users.noreply.github.com>
2023-12-20 18:28:34 -0600

    Added boilerplate for day 20

---
+++ src/com/noahgeren/adventofcode/days/xxiii/Day20.java
@@ -0,0 +1,19 @@
+package com.noahgeren.adventofcode.days.xxiii;
+
+import com.noahgeren.adventofcode.days.Day;
+
+public class Day20 extends Day {
+
+	@Override
+	public void loadResources() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String solve(boolean firstPart) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
